2ad726364438ad46b9da1e4b80b3ef34756e7e9a
unknown <zodraccir>
2017-10-13 15:12:43 +0200

    first commit

---
+++ files/main.js
@@ -0,0 +1,9 @@
+function menuFunction(){
+    var page=window.location.pathname.split("/").pop();
+    var x = document.getElementsByClassName("menu-item");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if(x[i].getElementsByTagName('a').getElementsByTagName('span').text==o)
+        
+    }
+}
